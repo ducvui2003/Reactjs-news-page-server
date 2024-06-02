f401b8ffa4938dba6898654fa59905590033eaac
Le Anh Duc <ducvui2003@gmail.com>
2024-06-02 23:01:48 +0700

    handle error xml

--- index.js
+++ index.js
@@ -11,25 +11,31 @@ var corsOptions = {
     origin: process.env.URL_CLIENT_PRODUCTION || process.env.URL_CLIENT_DEV,
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
-
+const createErrorXml = (message, status) => {
+    return `
+        <?xml version="1.0" encoding="UTF-8"?>
+        <error>
+            <message>${message}</message>
+            <status>${status}</status>
+        </error>
+    `;
+};
 app.get("/", cors(corsOptions), async (req, res, next) => {
     //   res.json({ msg: "This is CORS-enabled for a Single Route" });
+    res.set('Content-Type', 'application/xml');
     const {category} = req.query;
     if (!category) {
+        const errorXml = createErrorXml("Category query parameter is required", 400);
         return res
-            .status(400)
-            .json({error: "Category query parameter is required"});
+            .status(400).send(errorXml)
     }
 
     try {
         const response = await axios.get(`${process.env.URL_NEWS}${category}.rss`);
-        // const data = await xml2js.parseStringPromise(response.data, {
-        //   mergeAttrs: true,
-        // });
-        res.json(response);
+        res.status(200).send(response.data);
     } catch (err) {
-        console.log(err);
-        res.status(500).send("An error occurred while fetching the RSS feed.");
+        const errorXml = createErrorXml("An error occurred while fetching the RSS feed.", 500);
+        res.status(500).send(errorXml);
     }
 });
 
