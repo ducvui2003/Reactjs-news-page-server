496533565481f3174f670856c64c5255cfbe981b
ducvui2003 <ducvui2003@gmail.com>
2024-07-12 14:56:11 +0700

    change cors middle ware

--- index.js
+++ index.js
@@ -9,14 +9,12 @@ const PORT = process.env.PORT;
 const allowedOrigins = [process.env.URL_CLIENT_DEV, process.env.URL_CLIENT_PRODUCTION];
 console.log(allowedOrigins)
 const corsOptions = {
-    origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.indexOf(origin) === -1) {
-            const msg = 'The CORS policy for this site does not allow access from the specified origin.';
-            return callback(new Error(msg), false);
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
         }
-        return callback(null, true);
     },
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
